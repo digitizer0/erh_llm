6fc384493265a30f6bd28375804f589766e8fb57
Rikard Hermansson <rikard@erh.se>
2025-07-26 09:38:40 +0000

    Tystat clippy m.m.

--- src/prompter.rs
+++ src/prompter.rs
@@ -1,15 +1,15 @@
 use crate::Query;
 
-
+#[allow(unused)] 
 pub(crate) fn create_prompt(
     query: &mut Query,
     retrieved_chunks: &(Vec<String>, Vec<String>),
-) -> () {
+) {
     query.constraint = Some("You are a helpful assistant. You have access to the following context:".to_string());
     query.style = Some("If you are unsure about a question, make sure you ask for clarification before moving forward".to_string());
     query.context = retrieved_chunks.0.clone()
         .iter()
-        .map(|chunk| format!("{}\n", chunk))
+        .map(|chunk| format!("{chunk}\n"))
         .collect::<String>();
 }
 
@@ -23,9 +23,6 @@ pub(crate) fn create_prompt_summarized(
     let style = "If you are unsure about a question, make sure you ask for clarification before moving forward";
     let context = retrieved_chunks;
     // Combine the retrieved chunks with the user message
-    let augmented_message = format!("{constraint}\n\nContext:{}\n\nUser Query:\n{}\n{style}",
-        context,       
-        message
-    );
+    let augmented_message = format!("{constraint}\n\nContext:{context}\n\nUser Query:\n{message}\n{style}");
     augmented_message
 }
